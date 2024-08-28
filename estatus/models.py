e004aa99dd3ccdf68148f499f21ade7e388c3010
Luis Aguila <laguila@fletes-mexico.com>
2024-08-28 16:43:29 -0600

    la wea del script que falla :´v

--- estatus/models.py
+++ estatus/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 class NewTrip(models.Model):
-    order = models.CharField(max_length=255, default="Colocar orden", blank=True, null=True)
-    trailer = models.CharField(max_length=6, default="", blank=True, null=True)
+    orden = models.CharField(max_length=255, default="Colocar orden", blank=True, null=True)
+    carro = models.CharField(max_length=6, default="", blank=True, null=True)
     remolque = models.CharField(max_length=10, default="", blank=True, null=True)
-    posicion = models.CharField(max_length=255, default="", blank=True, null=True)
-    destino = models.CharField(max_length=255, default="", blank=True, null=True)
-    distancia = models.CharField(max_length=255, default="", blank=True, null=True)
-    tiempo_estimado = models.CharField(max_length=255, default="", blank=True, null=True)
+    origen = models.CharField(max_length=255, default="", blank=True, null=True)
+    destination = models.CharField(max_length=255, default="", blank=True, null=True)
+    distance = models.CharField(max_length=255, default="", blank=True, null=True)
 
     def __str__(self):
         return self.order
--- estatus/views.py
+++ estatus/views.py
@@ -1,36 +1,43 @@
+from django.views import View
 from django.shortcuts import render, redirect
-from django.utils import timezone
+from django.http import JsonResponse
 #from .models import CreateOrder, CreateClient  # Reemplaza 'TuModelo' por el nombre de tu modelo
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
 from .models import *
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 def register(request):
     return render(request, "index.html")
 
-def create_new_trip(request):
+@csrf_exempt
+def calculate_distance(request):
     if request.method == 'POST':
-        order = request.POST.get('order')
-        trailer = request.POST.get('trailer')
-        remolque = request.POST.get('remolque')
-        posicion = request.POST.get('posicion')
-        destino = request.POST.get('destino')
-        distancia = request.POST.get('distancia')
-        tiempo_estimado = request.POST.get('tiempo_estimado')
+        try:
+            data = json.loads(request.body)
+            origin = data.get('origin')
+            destination = data.get('destination')
+            orden = data.get('orden')
+            carro = data.get('carro')
+            remolque = data.get('remolque')
+            distance = data.get('distance')
 
-        # Crear una nueva instancia de NewTrip y guardar los datos
-        new_trip = NewTrip(
-            order=order,
-            trailer=trailer,
-            remolque=remolque,
-            posicion=posicion,
-            destino=destino,
-            distancia=distancia,
-            tiempo_estimado=tiempo_estimado
-        )
-        new_trip.save()
+            # Guarda en la base de datos
+            location_distance = NewTrip(
+                origin=origin,
+                destination=destination,
+                distance=distance,
+                orden=orden,
+                carro=carro,
+                remolque=remolque
+            )
+            location_distance.save()
 
-        return redirect('home')  # Redirige a una página de éxito después de guardar
-
-    return render(request, 'index.html')
+            return JsonResponse({'status': 'success'})
+        except json.JSONDecodeError:
+            return JsonResponse({'status': 'error', 'message': 'Invalid JSON'}, status=400)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid method'}, status=405)
